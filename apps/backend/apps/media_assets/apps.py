b13ca16c98b11de096e378cdf40b615a087ff853
salazar <lalo@gmail.com>
2026-04-05 22:54:00 -0600

    fix: remove venv from repo

--- apps/backend/apps/media_assets/apps.py
+++ apps/backend/apps/media_assets/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MediaAssetsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'media_assets'
+    name = 'apps.media_assets'
