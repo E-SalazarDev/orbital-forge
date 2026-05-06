c5a10c5194221217240e9db2d6d2c3f56e85ace1
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-05-05 22:48:02 -0600

    open camera cv2

--- apps/services/ml/src/capture/01_open_camera.py
+++ apps/services/ml/src/capture/01_open_camera.py
@@ -1,7 +1,22 @@
-import cv2 as cv 
+# pixel: [220, 170 130]
+# matriz de pixeles  frame[0][0] : [220, 170 130]
 
-img = cv.imread('hands.jpeg')
+import cv2
 
-cv.imshow('hands', img)
+camera = cv2.VideoCapture(0)
 
-cv.waitKey(0)
+while camera.isOpened():
+    ret, frame = camera.read()
+
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+
+    cv2.imshow("camara", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
