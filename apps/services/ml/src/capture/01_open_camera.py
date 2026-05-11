993860df9a8a2229a0f235761b135e9ac164e5a5
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-05-10 18:25:07 -0600

    hands

--- apps/services/ml/src/capture/01_open_camera.py
+++ apps/services/ml/src/capture/01_open_camera.py
@@ -2,52 +2,30 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# 1. Cargar la imagen
-img_color = cv.imread('hands.jpeg')
-# Convertimos a RGB para que Matplotlib muestre los colores correctos
-img_rgb = cv.cvtColor(img_color, cv.COLOR_BGR2RGB)
 
-# 2. Procesos de Intensidad (Gris y Gaussian)
-img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
-img_blur = cv.medianBlur(img_gray, 5)
-th2 = cv.adaptiveThreshold(img_blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                           cv.THRESH_BINARY, 11, 2)
+img = cv.imread('hands.jpeg')
 
-# 3. Proceso HSV
-img_hsv = cv.cvtColor(img_color, cv.COLOR_BGR2HSV)
-bajo_piel = np.array([0, 30, 60])
-alto_piel = np.array([20, 150, 255])
+cv.imshow("hands", img)
 
-# Creamos la máscara (Mano blanca, fondo negro para findContours)
-mascara_piel = cv.inRange(img_hsv, bajo_piel, alto_piel)
+# def rescaleFrame(frame, scale=0.75):
+#     width = int(frame.shape[1] * scale)
+#     height = int(frame.shape[0] * scale)
 
-# Inversión para el ejemplo visual (Fondo blanco, mano negra)
-mascara_fondo = cv.bitwise_not(mascara_piel)
+# convertimos a hsv
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-# --- NUEVO: EXTRAER CONTORNOS DESDE LA MÁSCARA HSV ---
-# Usamos 'mascara_piel' porque los contornos se buscan sobre objetos blancos
-contornos, _ = cv.findContours(mascara_piel, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+# definir el rango de color 
+lower_red = np.array([0, 100, 100])
 
-# Dibujamos los contornos sobre una copia de la imagen original
-img_con_contornos = img_rgb.copy()
-cv.drawContours(img_con_contornos, contornos, -1, (0, 255, 0), 3) # Verde
+upper_red = np.array([10,255,255])
 
-# 4. Visualización en cuadrícula 2x2
-titles = ['Gris Original', 'Gaussian Thresholding', 
-          'HSV Máscara (Fondo)', 'HSV Contornos sobre Original']
-images = [img_gray, th2, mascara_fondo, img_con_contornos]
+# crear la mascara
+mask = cv.inRange(hsv, lower_red, upper_red)
 
-plt.figure(figsize=(12, 8))
+# aplicar la máscara a la imagen original
+resultado  =  cv.bitwise_and(img, img, mask=mask)
 
-for i in range(4):
-    plt.subplot(2, 2, i+1)
-    # Las primeras 3 son escala de grises, la última es color
-    if i < 3:
-        plt.imshow(images[i], 'gray')
-    else:
-        plt.imshow(images[i])
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
-
-plt.tight_layout()
-plt.show()
+cv.imshow("Mask", mask)
+cv.imshow("Result", resultado)
+cv.waitKey(0)
+cv.destroyAllWindows()
