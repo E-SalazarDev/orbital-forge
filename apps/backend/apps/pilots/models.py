c5d3d3b8485b4030a0f4eae81106f152298a328a
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-05-11 21:04:46 -0600

    backend pilots models

--- apps/backend/apps/pilots/models.py
+++ apps/backend/apps/pilots/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class GameSession(models.Model):
+    IdGameSession = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    SessionKey = models.CharField(max_length=64)
+    IsGuest = models.BooleanField(default=True)
+    CreateAt = models.DateTimeField(auto_now=True)
+    LastSeenAt = models.DateTimeField(auto_now=True)
+    
+    # UserId
+    
