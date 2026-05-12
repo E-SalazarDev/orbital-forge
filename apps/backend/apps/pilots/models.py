8d61b1a7ea850dfff1fe879557c4fa5c97b6d9bf
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-05-11 23:28:58 -0600

    backend models PlayerRun FK

--- apps/backend/apps/pilots/models.py
+++ apps/backend/apps/pilots/models.py
@@ -22,3 +22,29 @@ class GameSession(models.Model):
         return self.SessionKey
     # UserId
     
+class PlayerRun(models.Model):
+    
+    class Status(models.TextChoices):
+        ACTIVE = "activate", "Activate",
+        COMPLETED = "completed", "Completed"
+        ABANDONED = "abandoned", "Abandoned"
+    
+    IdPlayerRun = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Nickname = models.CharField(max_length=64, blank=False)
+    Level  = models.PositiveIntegerField(default=1)
+    Xp = models.PositiveBigIntegerField(default=0)
+    credits = models.PositiveIntegerField(default=0)
+    Stataus = models.CharField(max_length=30, choices=Status.choices, default=Status.ACTIVE)
+    
+    CreatedAt = models.DateTimeField(auto_now=True)
+    UpdatedAt = models.DateField(auto_now=True)
+    
+    game_session = models.OneToOneField(
+        GameSession,
+        on_delete=models.CASCADE,
+        related_name="player_run"
+    )
+    
+    def __str__(self):
+        return self.Nickname or  f"PlayerRun {self.IdPlayerRun}"
+        
