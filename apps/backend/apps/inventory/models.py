3d7c7bde57095e465e67af9e321cbc7bd0d45dd5
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-05-13 22:43:30 -0600

    inventory

--- apps/backend/apps/inventory/models.py
+++ apps/backend/apps/inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ShipEquipament(models.Model): 
+    IdShipEquipament = models.UUIDField(primary_key=True, default=False)
+    Slot = models.CharField(max_length=60)
+    EquippedAt = models.DateTimeField(auto_now_add=True)
+    
